8d05cf2da4991a6e12194d773a27ed5518f24b75
Carlos William <carloswtmachado@gmail.com>
2022-05-30 12:36:52 -0300

    login

--- src/Componentes/Login/Login.js
+++ src/Componentes/Login/Login.js
@@ -1,19 +1,41 @@
 import logo from "../../Assets/logo.png"
 import { Link } from "react-router-dom";
-//import Input from "./Input";
-//import Button from "./Button";
+import { useState } from 'react';
+import axios from 'axios';
+import React from 'react';
 import styled from "styled-components";
 
 export default function Login () {
+	const [form, setForm] = useState({
+		email: '',
+		password: '',
+	});
+
+	function handleForm (e) {
+		setForm({...form, [e.target.name]: e.target.value,});
+	}
+
+	function doSubmit (event) {
+		event.preventDefault();
+		console.log(form.email);
+		console.log(form.password);
+
+		const promisse = axios.post('https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/login', {email: form.email, password: form.password});
+		promisse.then((response) => console.log(response.data))
+		.catch(error => console.log(error));
+		
+		
+	}
+
 	return (
 		<LoginContainer>
 			<Logo src={logo} alt={'logo'} height={178} width={180}/>
-			<div>
-				<Input type='email' placeholder='email'/>
-				<Input type='password' placeholder='senha'/>
-				<button>Entrar</button>
-				<Link to='/cadastro'>Não tem uma conta? Cadastre-se!</Link>
-			</div>
+			<form onSubmit={doSubmit}>
+				<Input type='email' name='email' placeholder='email' value={form.email} onChange={handleForm} required/>
+				<Input type='password' name='password' placeholder='senha' value={form.password} onChange={handleForm} required/>
+				<Button>Entrar</Button>
+				<StyledLink to='/cadastro'>Não tem uma conta? Cadastre-se!</StyledLink>
+			</form>
 		</LoginContainer>
 	);
 }
@@ -39,9 +61,9 @@ const Input = styled.input`
 	width: 303px;
 	border: 1px solid #D5D5D5;
 	background-color: #FFFFFF;
-	padding: 10px;
+	padding-left: 10px;
 	margin-bottom: 6px;
-	margin-left: 30px;
+	margin-left: 36px;
 	border-radius: 5px;
 
 	::placeholder {
@@ -51,13 +73,26 @@ const Input = styled.input`
 	}
 `;
 
-/*
+const Button = styled.button`
+	height: 45px;
+	width: 303px;
+	margin-left: 36px;
+	background-color: #52B6FF;
+	color: #FFFFFF;
+	font-family: 'Lexend Deca', sans-serif;
+	font-size: 21px;
+	border-radius: 4.63636px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+`;
+
 const StyledLink = styled(Link)`
-    height: 40px;
+	font-size: 14px;
+	margin-top: 25px;
     display: flex;
     justify-content: center;
     align-items: center;
     color: #52B6FF;
     font-family: 'Lexend Deca', sans-serif;
 `;
- */
