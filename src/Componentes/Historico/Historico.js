88f332939b93690857dbbca4c30a340bfc4eb02a
Carlos William <carloswtmachado@gmail.com>
2022-05-30 19:15:25 -0300

    historico

--- src/Componentes/Historico/Historico.js
+++ src/Componentes/Historico/Historico.js
@@ -1,13 +1,33 @@
 import Header from "../Header";
 import Footer from "../Footer";
 
+import React from 'react';
+import styled from 'styled-components';
+
 export default function Historico () {
 	return (
 		<>
 			<Header />
-			<p>Histórico</p>
-			<p>Em breve, você poderá ver o histórico dos seus hábitos, aqui!</p>
+			<Titulo>Histórico</Titulo>
+			<Texto>Em breve, você poderá ver o histórico dos seus hábitos, aqui!</Texto>
 			<Footer />
 		</>
 	);
 }
+
+const Titulo = styled.p`
+	margin-top: 90px;
+	margin-left: 17px;
+	margin-bottom: 17px;
+	font-family: 'Lexend Deca', sans-serif;
+	font-size: 23px;
+	color: #126BA5;
+`;
+
+const Texto = styled.p`
+	font-family: 'Lexend Deca', sans-serif;
+	font-size: 18px;
+	color: #666666;
+	margin-left: 17px;
+	margin-right: 17px;
+`;
