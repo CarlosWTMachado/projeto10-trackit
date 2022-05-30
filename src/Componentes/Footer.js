0b154ce41fa37c93a22639d281a6ca7b7874911e
Carlos William <carloswtmachado@gmail.com>
2022-05-30 20:11:21 -0300

    footer

--- src/Componentes/Footer.js
+++ src/Componentes/Footer.js
@@ -26,41 +26,6 @@ export default function Footer () {
 		</FooterBar>
 	);
 }
-/*
-import { CircularProgressbar } from 'react-circular-progressbar';
-import { buildStyles } from 'react-circular-progressbar';
-import { useNavigate } from 'react-router-dom';
-import { useState } from 'react';
-import './components/Footer.css';
-import styled from "styled-components";
-
-export default function Footer () {
-    const [value, setValue] = useState(0);
-    const navigate = useNavigate();
-    return (
-        <FooterBar>
-            <button className={'side-footer-button'} onClick={(() => {navigate('/habits')})}>Hábitos</button>
-            <button className={'today-circular-progress-bar'} onClick={(() => {navigate('/today')})}>Hoje</button>
-            <button className={'side-footer-button'} onClick={(() => {navigate('/history')})}>Histórico</button>
-        </FooterBar>
-    );
-}
-
-<Example label="Background">
-      <CircularProgressbar
-        value={percentage}
-        text={`${percentage}%`}
-        background
-        backgroundPadding={6}
-        styles={buildStyles({
-          backgroundColor: "#3e98c7",
-          textColor: "#fff",
-          pathColor: "#fff",
-          trailColor: "transparent"
-        })}
-      />
-    </Example>
-*/
 
 const Circular = styled(CircularProgressbar)`
 	margin-bottom: 60px;
